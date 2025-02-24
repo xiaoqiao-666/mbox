91bf99c52dcaab5e96f0ca148f3a301a89cf469c
xiaoqiao-666 <3151930548@qq.com>
2025-02-24 15:47:55 +0800

    App

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {public static void main(String[] args) {
+    SliceOHeaven pizzeria = new SliceOHeaven();
+    pizzeria.takeOrder("PIZZA-1001", "1 Margherita Pizza, 2 Garlic Bread, 1 Coke");
+    }   
+}
